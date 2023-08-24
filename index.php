8e97c044772fcc1ddc89fdaddfe4453587207a73
KevinMan46 <k.asca@unas.edu.pe>
2023-08-24 13:01:55 -0500

    best menues

--- index.php
+++ index.php
@@ -41,66 +41,21 @@
         }
     </style>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Inicio</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-            <a class="nav-link" href="#">Instrumentos de gestion ambiental<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="eventos.html">Eventos</a>
-            </li>
-            
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Investigaciones
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Investigaciones de pregrado</a>
-                <a class="dropdown-item" href="#">Investigaciones de posgrado</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Revista de investigación</a>
-            </div>
-            </li>
-             <li class="nav-item">
-            <a class="nav-link" href="#">Reporte de sostenibilidad</a>
-            </li>
-             <li class="nav-item">
-            <a class="nav-link" href="#">Voluntariado ambiental unasino</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        </div>
-    </nav>
+    <?php
+    include("nav.php");
+    ?>
 
     <div class="container text-center mt-5 mb-2">
         <h1 class="mb-0">Comité Ambiental UNAS</h1>
         <div class="dj-banner">
-    <div class="dj-bg mb-4 mt-4">
-        <img src="img/miembros/gest_ambiental.jpg" alt="" height="365px">
-        </div>
-    </div> 
-    <h4>¿QUIÉNES SOMOS?</h4>  
-     <span>Somos una universidad comprometida con la conservación del ambiente y su entorno que busca fortalecer la conciencia ambiental de la población universitaria</span>         
-</div>
-        
-      
-        <div class="row">
-            <div class="col-md-12">
-                <img src="" alt="">
+            <div class="dj-bg mb-4 mt-4">
+                <img src="img/miembros/gest_ambiental.jpg" alt="" height="365px">
             </div>
-        </div>
+        </div> 
+        <h4>¿QUIÉNES SOMOS?</h4>  
+        <span>Somos una universidad comprometida con la conservación del ambiente y su entorno que busca fortalecer la conciencia ambiental de la población universitaria</span>         
+    </div>
+
     </div>
     <div class="container mt-3">
         <div class="row">
---
+++ instrumentos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión Ambiental UNAS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <style>
+        body {
+        background: #eee;
+        }
+
+        .name {
+        color: blue;
+        }
+
+        .work {
+        font-weight: bold;
+        font-size: 15px;
+        }
+
+        .about span {
+        font-size: 13px;
+        }
+
+        .v-profile {
+        color: blue;
+        cursor: pointer;
+        }
+
+        .box {
+        -webkit-box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+        -moz-box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+        box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+        }
+
+        .col-md-3 {
+        margin-top: 10px;
+        }
+    </style>
+
+    <?php
+    include("nav.php");
+    ?>
+
+    <div class="container text-center mt-5 mb-2">
+        <h1 class="mb-0">Instrumentos de Gestión Ambiental</h1>
+        <div class="dj-banner">
+            <div class="dj-bg mb-4 mt-4">
+                <img src="img/miembros/gest_ambiental.jpg" alt="" height="365px">
+            </div>
+        </div>        
+    </div>
+
+    </div>
+
+    <div class="container mt-3">
+        <div class="row">
+            <iframe src="https://portal.unas.edu.pe/instrumentos-gestion-ambiental" frameborder="0" width="100%" height="300" style="border:1px solid black;">
+
+            </iframe>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
